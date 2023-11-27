c4cd34cfeaf6775accb1039c5da2729171f98f68
micrimus <dbadiac@gmail.com>
2023-11-27 11:23:55 +0100

    my first commit

---
+++ Balanceatuvida/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Balanceatuvida.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Balanceatuvida.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var Title = new Title()
+            {
+                MainTitle = "Renace desde tu interior",
+                Description = "Aprende a equilibrar tu rueda de la vida"
+            };
+
+            return View(Title);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Balanceatuvida/Models/Title.cs
@@ -0,0 +1,9 @@
+﻿namespace Balanceatuvida.Models
+{
+    public class Title
+    {
+        public string? MainTitle {  get; set; }
+
+        public string? Description { get; set; }
+    }
+}
